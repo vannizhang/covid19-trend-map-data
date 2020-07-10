754853d7b04dcd4c38ee8d6407b389d4fb064755
Jinnan Zhang <vannizhang@gmail.com>
2020-07-10 09:52:08 -0700

    update download-covid19-data.ts

--- src/download-covid19-data.ts
+++ src/download-covid19-data.ts
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 import axios from 'axios';
+import USCounties from './US-Counties.json';
 
-const OUTPUT_JSON = path.join(__dirname, '/public/data.json');
+const PUBLIC_FOLDER_PATH = path.join(__dirname, '../public');
+const OUTPUT_JSON = path.join(PUBLIC_FOLDER_PATH, 'data.json');
 
 const fetchCovid19Data4USCounties = ()=>{
 
@@ -10,20 +12,30 @@ const fetchCovid19Data4USCounties = ()=>{
 
 const startUp = async()=>{
 
+    makeFolder(PUBLIC_FOLDER_PATH);
+
     // const confirmedCases7DaysAve = await getConfirmedCases7DaysAve();
     // console.log(confirmedCases7DaysAve)
 
     // if(confirmedCases7DaysAve){
     //     writeToJson(confirmedCases7DaysAve, OUTPUT_JSON_CONFIRMED_CASES_7_DAY_AVG);
     // }
+
+    writeToJson(USCounties, OUTPUT_JSON);
 };
 
 const writeToJson = (data, outputPath)=>{
     const json = JSON.stringify(data);
     // const outputFileName = 'data.json';
     fs.writeFile(outputPath, json, 'utf8', ()=>{
-        console.log('data.json is updated and saved:', new Date(), '\n');
+        console.log(new Date(), `data.json is updated and saved: ${outputPath}`, '\n');
     });
+};
+
+const makeFolder = (dir)=>{
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
 };
 
 const average = (values) => values.reduce((a, b) => a + b) / values.length;
