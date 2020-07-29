e819a2b6ccdeae0b21eef4e0b493c36db85d13b9
vannizhang <vannizhang@gmail.com>
2020-07-29 23:42:07 +0000

    edit starter.sh

--- starter.sh
+++ starter.sh
@@ -3,5 +3,5 @@
 if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then
         export PATH=/usr/local/bin:$PATH
-        forever start /root/covid19-trend-map-data/dist/server.js >> /root/covid19-trend-map-data/log.txt 2>&1
+        forever start /root/covid19-trend-map-data/dist/covid19-data-server.js >> /root/covid19-trend-map-data/log.txt 2>&1
 fi
