9e13e832bad0aab9626430b5510fceafcd9abec2
Jinnan Zhang <vannizhang@gmail.com>
2020-07-30 09:29:35 -0700

    update express.static path

--- dist/covid19-data-server.js
+++ dist/covid19-data-server.js
@@ -8,16 +8,21 @@ const fs = require('fs');
 const path = require("path");
 const app = express_1.default();
 const port = process.env.PORT || 8500;
+const parentDir = path.resolve(__dirname, '..');
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.use(express_1.default.static(path.resolve(__dirname, 'public')));
+app.use(express_1.default.static(path.join(parentDir, 'public')));
 app.listen(port, function () {
     console.log('app listening on port ' + port);
-    console.log(path.resolve(__dirname, 'public'));
 });
 app.get('/', function (req, res) {
     res.json({ "version": "0.1" });
+    console.log(path.resolve(__dirname, 'public'));
+    console.log(path.join(__dirname, 'public'));
+    console.log(__dirname + 'public');
+    console.log(path.join(parentDir, 'public'));
+    console.log(parentDir);
 });
--- src/covid19-data-server.ts
+++ src/covid19-data-server.ts
@@ -4,8 +4,7 @@ const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 8500;
-
-import USStates from '../public/us-states.json';
+const parentDir = path.resolve(__dirname, '..');
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -13,23 +12,19 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.join(parentDir, 'public')));
 
 app.listen(port, function () {
-    const parentDir = path.resolve(__dirname, '..');
     console.log('app listening on port ' + port);
-    console.log(path.resolve(__dirname, 'public'));
-    console.log(path.join(__dirname, 'public'));
-    console.log(__dirname + 'public');
-    console.log(path.join(parentDir, 'public'));
 });
 
 app.get('/', function(req, res) {
     res.json({"version": "0.1"});
     // res.sendFile(path.join(__dirname + '/index.html'));
-});
 
-app.get('/foobar', function(req, res) {
-    res.json(USStates);
-    // res.sendFile(path.join(__dirname + '/index.html'));
+    console.log(path.resolve(__dirname, 'public'));
+    console.log(path.join(__dirname, 'public'));
+    console.log(__dirname + 'public');
+    console.log(path.join(parentDir, 'public'));
+    console.log(parentDir);
 });
