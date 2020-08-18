1fa70778043bc151d546fa73a64922660921cd99
Jinnan Zhang <vannizhang@gmail.com>
2020-08-17 18:00:45 -0700

    update exec-download-esri-linux-vm.sh

--- exec-download-esri-linux-vm.sh
+++ exec-download-esri-linux-vm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/node /root/covid19-trend-map-data/dist/download-covid19-data.js >> /root/covid19-trend-map-data/log.txt 2>&1
+/bin/node /root/covid19-trend-map-data/dist/download-covid19-data.js >> /root/covid19-trend-map-data/log.txt 2>&1
