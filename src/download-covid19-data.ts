8bdbd2dd907f1f5a7ab800fff0366392d40af744
Jinnan Zhang <vannizhang@gmail.com>
2020-09-28 15:46:37 -0700

    add cases and detahs of 100 days to latest numbers

--- src/download-covid19-data.ts
+++ src/download-covid19-data.ts
@@ -104,6 +104,8 @@ type COVID19LatestNumbersItem = {
     Deaths: number;
     NewCases: number;
     NewDeaths: number;
+    NewCases100Days: number;
+    NewDeaths100Days: number;
     Population: number;
     TrendType: COVID19TrendType | '';
 };
@@ -343,7 +345,8 @@ const saveToCOVID19LatestNumbers = (FIPS:string, Name:string, features: Covid19C
     // const indexOfLatestFeature = features.length - 1;
 
     const latestFeature = features[features.length - 1];
-    const feature7DaysAgo = features[features.length - 8]
+    const feature7DaysAgo = features[features.length - 8];
+    const features100DaysAgo = features[features.length - 101];
 
     const { attributes } = latestFeature;
 
@@ -360,6 +363,9 @@ const saveToCOVID19LatestNumbers = (FIPS:string, Name:string, features: Covid19C
 
     const newCasesPast7Days =  latestFeature.attributes.Confirmed - feature7DaysAgo.attributes.Confirmed;
     const newDeathsPast7Days =  latestFeature.attributes.Deaths - feature7DaysAgo.attributes.Deaths;
+    
+    const newCasesPast100Days =  latestFeature.attributes.Confirmed - features100DaysAgo.attributes.Confirmed;
+    const newDeathsPast100Days =  latestFeature.attributes.Deaths - features100DaysAgo.attributes.Deaths;
 
     COVID19LatestNumbers[FIPS] = {
         Name,
@@ -369,6 +375,9 @@ const saveToCOVID19LatestNumbers = (FIPS:string, Name:string, features: Covid19C
         // new cases and deaths of past 7 days
         NewCases: newCasesPast7Days,
         NewDeaths: newDeathsPast7Days,
+        // new cases and deaths of past 100 days
+        NewCases100Days: newCasesPast100Days,
+        NewDeaths100Days: newDeathsPast100Days,
         TrendType: USCountiesCOVID19TrendCategoryLookup[FIPS] || '',
     }
 };
